6b643f5043f5f2e29e6ea21bed3e21a46b8d06ab
Charith Jayasinghe <153707497+Nuwan-droid@users.noreply.github.com>
2025-06-09 14:44:46 +0530

    Initial setup

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
