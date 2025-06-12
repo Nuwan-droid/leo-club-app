8267e17b3a857f93dd6b512a1497ae98be20acc9
AhamedNismi-2000 <mnanismi2000@gmail.com>
2025-06-12 18:27:46 +0530

    done

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
+require('dotenv').config(); 
 const mongoose = require('mongoose');
-require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+  .catch((err) => console.error('MongoDB error:', err));
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+console.log(`Server is running on http://localhost:${PORT}`);
+
 });
