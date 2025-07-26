5004af811f68bf5c9043cff61280182e2314cf54
umayanga ranasinghe <umayangaranasinghe98@gmail.com>
2025-07-26 17:54:25 +0530

    new changes

--- client/src/Components/Pages/admin panel/users/child_components/UserTable.jsx
+++ client/src/Components/Pages/admin panel/users/child_components/UserTable.jsx
@@ -23,21 +23,21 @@ const UserTable = ({
               />
             </th>
             <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
-              Full Name
+              Image
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
+              First Name
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
+              last Name             
             </th>
             <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
               Email
             </th>
-            <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
-              Username
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
-              Status
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
+             <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
               Role
             </th>
-            <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
+             <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
               Actions
             </th>
           </tr>
