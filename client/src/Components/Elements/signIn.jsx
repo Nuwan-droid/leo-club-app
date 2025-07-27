09377ec7f758c6dc82321517b7af0af2819a436e
Charith Jayasinghe <153707497+Nuwan-droid@users.noreply.github.com>
2025-07-28 04:15:11 +0530

    small changes about login->rename>signIn

--- client/src/Components/Elements/signIn.jsx
+++ client/src/Components/Elements/signIn.jsx
@@ -39,7 +39,7 @@ export default function Login({ onClose}) {
     setError("");
 
     try {
-      const res = await fetch("http://localhost:5001/api/auth/login", {
+      const res = await fetch("http://localhost:5001/api/auth/signIn", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -8,6 +8,6 @@ const router = express.Router();
 router.post("/signup", signup);
 
 // --------------------- SIGNIN ---------------------
-router.post("/login", signIn);
+router.post("/signIn", signIn);
 
 export default router;
