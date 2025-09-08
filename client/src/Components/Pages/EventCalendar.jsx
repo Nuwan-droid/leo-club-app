e06eeb30d4273f9982a26235c300a6e19490991e
ThilinaSupun <thilisupun00011@gmail.com>
2025-09-08 15:03:32 +0530

    event calendar

--- client/src/Components/Pages/EventCalendar.jsx
+++ client/src/Components/Pages/EventCalendar.jsx
@@ -4,7 +4,6 @@ import CalendarGrid from '../Elements/EventCalendar/CalendarGrid';
 import EventNotes from '../Elements/EventCalendar/EventNotes';
 import useCalendarLogic from '../Elements/EventCalendar/useCalendarLogic';
 
-
 const EventCalendar = () => {
   const {
     year,
@@ -19,24 +18,32 @@ const EventCalendar = () => {
   } = useCalendarLogic();
 
   return (
-    <div className="max-w-4xl mx-auto p-6 bg-gray-50 min-h-screen mt-20">
-      <div className="bg-white rounded-lg shadow-sm border border-gray-200 overflow-hidden">
-        <CalendarHeader
-          currentMonth={month}
-          currentYear={year}
-          onPrevious={goToPreviousMonth}
-          onNext={goToNextMonth}
-        />
+    <div className="max-w-7xl mx-auto p-6 bg-gray-50 min-h-screen mt-20">
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
+        {/* Left Sidebar - Event Notes */}
+        <div className="lg:col-span-1 order-2 lg:order-1">
+          <EventNotes events={events} />
+        </div>
 
-        <CalendarGrid
-          weeks={weeks}
-          getEventsForDate={getEventsForDate}
-          isToday={isToday}
-          monthName={months[month - 1] || 'December'}
-        />
+        {/* Main Calendar */}
+        <div className="lg:col-span-3 order-1 lg:order-2">
+          <div className="bg-white rounded-lg shadow-lg border border-gray-100 overflow-hidden">
+            <CalendarHeader
+              currentMonth={month}
+              currentYear={year}
+              onPrevious={goToPreviousMonth}
+              onNext={goToNextMonth}
+            />
+
+            <CalendarGrid
+              weeks={weeks}
+              getEventsForDate={getEventsForDate}
+              isToday={isToday}
+              monthName={months[month - 1] || 'December'}
+            />
+          </div>
+        </div>
       </div>
-
-      <EventNotes events={events} />
     </div>
   );
 };
